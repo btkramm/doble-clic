b0b8f41ffb65fc7ffdc1793e804d6c698777459c
Benjamín Kramm <benjakramm@fintual.com>
2022-10-03 15:28:44 -0300

    feat: use authentication provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,12 @@ import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
 
+import AuthenticationProvider from '@/modules/authentication/contexts/authentication-provider';
+
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <AuthenticationProvider>
+    <Component {...pageProps} />
+  </AuthenticationProvider>
 );
 
 export default MyApp;
